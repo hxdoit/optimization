c47629dfb8ac0c7283808c9bbf2490da34ec0d17
huangxuan01 <huangxuan01@baidu.com>
2019-07-30 11:11:54 +0800

    curve fitting

---
+++ curve_fitting/fitting.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+M =  10 
+T = []
+
+with open('temp', 'r') as file_to_read:
+    while True:
+        lines = file_to_read.readline()
+        if not lines:
+            break
+        T.append(int(lines)) 
+N = len(T)
+        
+        
+
+X = np.zeros((N, M + 1))
+Tmat = np.mat(T)
+
+for i in range(N):
+    for j in range(M + 1):
+        if j == 0:
+            X[i][j] = 1 
+        else:
+            X[i][j] = i**j 
+Xmat = np.mat(X)
+
+W = (Xmat.T * Xmat).I * Xmat.T * Tmat.T
+Wa = W.getA()
+print Wa
+
+pltY1 = []
+for i in range(N):
+    temp = 0
+    for j in range(M + 1):
+        temp += Wa[j][0]*(i**j)
+    pltY1.append(temp)
+
+print sum(pltY1)-sum(T)
+
+plt.plot(range(N), pltY1, label="1", color="green", linewidth = 1, linestyle = '-')
+plt.plot(range(N), T, label="2", color="red", linewidth = 1, linestyle = '-')
+plt.xlabel("time(s)")
+plt.ylabel("distance(m)")
+plt.title("multiband")
+plt.show()
---
+++ curve_fitting/readme.txt
@@ -0,0 +1,3 @@
+docs:
+https://www.jianshu.com/p/eac4c7928b56
+https://www.cnblogs.com/jianxinzhou/p/4083921.html
